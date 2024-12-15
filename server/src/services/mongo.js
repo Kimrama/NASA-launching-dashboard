eae55ccf15e7fbcd0850ff70de0f6efbf088564e
Kimrama <kimrama0x@gmail.com>
2024-12-15 17:27:22 +0700

    use secret in env

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGO_URL =
-    "mongodb+srv://nasa-api:CbTVcZ0q6OrulX9D@nasacluster.b8cae.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
     console.log("MongoDB connection ready");
